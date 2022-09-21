c1a01d2e1a3e167fd2723941b4eebe99a6f6d8f6
BillyARMEL <billyessaga@yahoo.fr>
2022-09-21 23:16:05 +0000

    task9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-def to_uper(char):
-    if ord(char) >= 97 and ord(char) <= 122:
-        return (ord(char) - 32)
-    else:
-        return ord(char)
-
-
-def uppercase(string):
-    string_new = ""
-    for char in string:
-        string_new += "%c" % to_uper(char)
-    print("{:s}".format(string_new))
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
