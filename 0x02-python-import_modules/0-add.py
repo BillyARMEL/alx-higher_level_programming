3a5070ea26789a8016b94382275353ffe4ebde0b
BillyARMEL <billyessaga@yahoo.fr>
2022-09-24 15:17:16 +0000

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__": # for executing modules as scripts
+if __name__ == "__main__":  # for executing modules as scripts
 
     from add_0 import add
 a = 1
 b = 2
 
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
