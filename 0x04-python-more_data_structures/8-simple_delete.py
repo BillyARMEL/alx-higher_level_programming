445c85467ecbbea660687c2af2b89b12a51490bb
BillyARMEL <billyessaga@yahoo.fr>
2022-09-30 22:59:18 +0000

    task8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def print_sorted_dictionary(a_dictionary):
-
-    for x in sorted(a_dictionary):
-        print("{}: {}".format(x, a_dictionary[x]))
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
