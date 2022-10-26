f4976d86819b88b3e8fef1f6f663a7e9ac06a16e
BillyARMEL <billyessaga@yahoo.fr>
2022-10-26 23:31:44 +0000

    task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""my lookup module"""
+''' function that returns the list
+'''
 
 
 def lookup(obj):
-"""Return an attribute and method of an object"""
-        return dir(obj)
+    ''' function: lookup()
+    Returns a list object
+    '''
+    return dir(obj)
